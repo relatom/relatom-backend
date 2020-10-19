4fb41013237a7d9c6e059d0d1e78141b81261abc
Célestin Ballèvre <celestin.ballevre@gmail.com>
2020-10-19 09:45:09 +0200

    Add event

---
+++ tests/Feature/EventTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EventTest extends TestCase
+{
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/events');
+
+        $response->assertStatus(200);
+    }
+}
