870fc1348539edb7c5f410cf7a437abff2d50929
cballevre <celestin.ballevre@gmail.com>
2020-11-06 18:20:39 +0100

    WIP on auth

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DemoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* 
+			5 utilisateur demo :
+			- un admin 
+			- un organisateur
+			- un parent pratiquant + 2 enfants 
+			- un parent + 1 enfants 
+			- un pratiquant
+
+			10 événement + commentaire 
+		*/ 
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,26 @@ use App\Http\Controllers\MemberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', function( ){
+    $email = Request::get('email');
+    $password = Request::get('password');
+    
+
+    if (Auth::attempt([
+        'email' => $email,
+        'password' => $password
+    ])) {
+        return response()->json('', 204 );
+    }else{
+        return response()->json([
+            'error' => 'invalid_credentials'
+        ], 403);
+    }
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::resource('events', EventController::class);
 Route::resource('members', MemberController::class);
