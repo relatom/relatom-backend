90db250756ca0918cbf52afe4bc31bb1ebe3d96c
cballevre <celestin.ballevre@gmail.com>
2020-11-09 11:07:55 +0100

    WIP on login

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,6 @@ use App\Http\Controllers\MemberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', function( ){
-    $email = Request::get('email');
-    $password = Request::get('password');
-    
-
-    if (Auth::attempt([
-        'email' => $email,
-        'password' => $password
-    ])) {
-        return response()->json('', 204 );
-    }else{
-        return response()->json([
-            'error' => 'invalid_credentials'
-        ], 403);
-    }
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
